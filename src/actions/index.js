ef23df462b841da0167a3d7b3e8d814be5c915c0
adam-szczombrowski <adam.szczombrowski@gmail.com>
2018-10-04 08:04:56 +0200

    bind action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY='4a1874ab8e2058d1a6ba99e431ff2f18';
-const ROOT_URL = `http://api.openweather.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
